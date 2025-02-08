cd619d5fca0ef079e89f23fbc8069728a8487c56
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-02-08 22:28:03 +0700

    config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 class Config(object):
     # Mandotory
     APP_ID = int(os.environ.get("APP_ID", "26775695"))
-    API_HASH = os.environ.get("API_HASH", "b15bb60859bef151762fc5d9eb206c67"))
+    API_HASH = os.environ.get("API_HASH", "b15bb60859bef151762fc5d9eb206c67")) 
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7684599268:AAFf97fALKymo_Xi1hb3t0kiFP8Wmis1k8I"))
     LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1002482663527"))
     BOT_OWNER = int(os.environ.get("BOT_OWNER", "1200411908"))
